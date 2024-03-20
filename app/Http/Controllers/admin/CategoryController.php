29ac7d7cf4f39a0a3ea22b5233c7d6d12d18fc73
MonteryTrunn <Dangtrung@@03>
2024-03-20 11:04:24 +0700

    Upload Frontend 19/03

--- app/Http/Controllers/admin/CategoryController.php
+++ app/Http/Controllers/admin/CategoryController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\File;
 use Illuminate\Http\Request;
 use App\Models\Category;
 use Illuminate\Support\Facades\Validator;
-
+use Intervention\Image\Facades\Image;
 
 class CategoryController extends Controller
 {
@@ -92,20 +92,31 @@ class CategoryController extends Controller
             $category->status=$request->status;
             $category->save();
             $category->update($request->all());
+            $oldImage=$category->image;
             if(!empty($request->image_id)){
                 $tempImage=TempImage::find($request->image_id);
                 $extArray=explode('.',$tempImage->name);
                 $ext=last($extArray);
                 $newImageName=$category->id.'.'.$ext;
+                $sPath=public_path() . '/tempImage/' . $tempImage->name;
+                $dPath=public_path() . '/images/img_new/' . $newImageName;
+                File::copy($sPath, $dPath);
 
-                // Delete the old image if it exists
-                if (File::exists(public_path() . '/images/' . $category->image)) {
-                    File::delete(public_path() . '/images/' . $category->image);
-                }
+
+                    $dPath = public_path() . '/images/img_new/thumb/' . $newImageName;
+                    $img = Image::make($sPath);
+                    $img->fit(450, 600, function ($constraint) {
+                        $constraint->upsize();
+                    });
+                    $img->save($dPath);
+
+
 
                 $category->image=$newImageName;
                 $category->save();
-                File::move(public_path() . '/tempImage/' . $tempImage->name, public_path() . '/images/' . $newImageName);
+                //Delete Old Image
+//                File::delete(public_path(). '/images/img_new/thumb/' . $oldImage);
+//                File::delete(public_path(). '/images/img_new/' . $oldImage);
             }
             $request->session()->flash('success','Category updated successfully');
             return redirect()->route('categories.edit', ['category' => $category->id]);
@@ -125,6 +136,26 @@ class CategoryController extends Controller
         $delete = Category::find($id)->delete();
 
         return redirect()->route('categories.list')->with('success', 'Category deleted successfully');
+    }
+
+    public function destroyImage($id,Request $request){
+        $category = Category::find($id);
+        if (empty($category)) {
+            $request->session()->flash('error','Category not found');
+           return  response()->json([
+               'status'=>'false',
+               'notFound'=>'true',
+               'message'=>'Category not found'
+           ], 404);
+
+        }
+        File::delete(public_path(). '/images/img_new/thumb/' . $category->image);
+        File::delete(public_path(). '/images/img_new/' . $category->image);
+        $category->delete();
+        $request->session()->flash('success','Category deleted successfully');
+        return reponse()->json([
+            'status'=>'true',
+            'success'=>'Category deleted successfully']);
     }
 
 }
